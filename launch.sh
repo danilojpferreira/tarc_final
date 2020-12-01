e3b1eb7923562b7ace6c38591525696fb13ffc1e
Danilo Pereira <danilo@7waves.me>
2020-12-01 11:17:49 -0300

    fix configs

--- launch.sh
+++ launch.sh
@@ -24,6 +24,7 @@ sudo apt install -y kubelet kubeadm kubectl python
 
 if ["$0" == "master"]; then 
         # Install and configure AWS account (need to upload config file)
+        sudo apt install -y unzip
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         unzip awscliv2.zip
         sudo ./aws/install
