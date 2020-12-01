d98c224f3dbff9fc830ce33bd79f128bb8fdda17
Danilo Pereira <danilo@7waves.me>
2020-12-01 11:08:15 -0300

    fix launch

--- launch.sh
+++ launch.sh
@@ -8,21 +8,21 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent softwar
 sudo apt update - y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update -y
+sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Install Ansible
-sudo apt update -y
+sudo apt update
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
 #Install Kubernets
-sudo apt update -y
+sudo apt update
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt install -y kubelet kubeadm kubectl python
 
 
-if [$1 === 'master'] 
+if ["$0" == "master"] 
     then 
         # Install and configure AWS account (need to upload config file)
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
