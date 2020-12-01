4e8a6dff7816abb53eb4507ea58a637f712d552b
Danilo Pereira <danilo@7waves.me>
2020-12-01 11:11:55 -0300

    fix launch

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Update packages
-sudo apt update - y && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 #Install Docker packages
 sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-sudo apt update - y
+sudo apt update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
