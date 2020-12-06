326d9141ceb615eaa99a5baa8c3b5548bd6b0283
Danilo Pereira <danilo@7waves.me>
2020-12-05 22:06:19 -0300

    sleep added

--- scale.sh
+++ scale.sh
@@ -42,6 +42,7 @@ then
         random=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 3)
         aws ec2 run-instances --region $region --image-id ami-0885b1f6bd170450c --count 1 --instance-type $instanceType --key-name $keyName --security-group-ids $securityGroups --subnet-id $subnetId --tag-specifications "ResourceType=instance,Tags=[{Key=hash,Value=$random},{Key=TARC,Value=true},{Key=nodeType,Value=worker}]" --user-data file://launch.sh
         instance=$(aws ec2 describe-instances --region $region --filters "Name=tag:hash,Values=$random" --query Reservations[*].Instances[*].[InstanceId] --output text)
+        sleep 60
         aws elbv2 register-targets --region $region --target-group-arn arn:aws:elasticloadbalancing:us-east-1:807587852252:targetgroup/TARC-TARGET-GROUP/a499ba73d38018c2 --targets Id=$instance
     done
 
